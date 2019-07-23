214d298dba733f2d746daaf9c247975933d25c57
Jason Yingling <jyingling@hlkagency.com>
2019-07-22 19:24:20 -0500

    initial commit

---
+++ stl-wp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Stl Wp
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     stl-wp
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Stl_Wp
+ */
+
+// Your code starts here.
