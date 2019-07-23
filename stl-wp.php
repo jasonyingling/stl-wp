47348b322787fcc0509d5169a1e7dd169ec9831a
Jason Yingling <jyingling@hlkagency.com>
2019-07-22 19:46:10 -0500

    push revert

--- stl-wp.php
+++ stl-wp.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name:     Stl Wp
- * Plugin URI:      New Branch
- * Description:     And also this
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
  * Author:          YOUR NAME HERE
  * Author URI:      YOUR SITE HERE
  * Text Domain:     stl-wp
