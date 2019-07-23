288ecee5bc012b479785ce1a76ff31d15713747f
Jason Yingling <jyingling@hlkagency.com>
2019-07-22 19:29:06 -0500

    version 0.1.2

--- stl-wp.php
+++ stl-wp.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     stl-wp
  * Domain Path:     /languages
- * Version:         0.1.1
+ * Version:         0.1.2
  *
  * @package         Stl_Wp
  */
